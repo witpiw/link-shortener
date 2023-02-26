5548e252015911e37bdd6541d33148029bdee893
witpiw <piweckiwit@gmail.com>
2023-02-26 22:21:23 +0100

    feat: add basic redirect page

---
+++ pages/[slug].tsx
@@ -0,0 +1,71 @@
+import { Database } from "../types/supabase";
+
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import type { GetServerSidePropsContext } from "next";
+import { useEffect, useState } from "react";
+import { Flex, Text } from "@chakra-ui/react";
+
+function Redirect(props: { redirect_to: string }) {
+	const REDIRECT_WAITING_TIME = 3000;
+
+	const [timeLeft, setTimeLeft] = useState(REDIRECT_WAITING_TIME / 1000);
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setTimeLeft((timeLeft) => timeLeft - 1);
+		}, 1000);
+
+		const timeoutId = setTimeout(() => {
+			location.href = props.redirect_to;
+		}, REDIRECT_WAITING_TIME);
+
+		return () => {
+			clearInterval(intervalId);
+			clearTimeout(timeoutId);
+		};
+	}, []);
+
+	return (
+		<>
+			<Text pt={20} h="10vh" textAlign={"center"} fontSize={"xl"}>
+				You will be redirected in {timeLeft}
+			</Text>
+			<Flex
+				h="90vh"
+				justifyContent={"center"}
+				alignItems={"center"}
+				direction={"column"}
+			>
+				<Text>(annoying ads)</Text>
+			</Flex>
+		</>
+	);
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+	if (!context.params) return;
+
+	const supabase = createServerSupabaseClient<Database>(context);
+
+	const { data, error } = await supabase
+		.from("links")
+		.select("redirect_to")
+		.eq("link_slug", context.params.slug);
+
+	if (error || !data[0]) {
+		return {
+			redirect: {
+				permanent: false,
+				destination: "/",
+			},
+		};
+	}
+
+	const redirect_to = data[0].redirect_to;
+
+	return {
+		props: { redirect_to },
+	};
+}
+
+export default Redirect;
