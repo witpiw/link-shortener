0886e7e9b5b9cda220a7014f237314b762d4edfd
witpiw <piweckiwit@gmail.com>
2023-02-26 10:14:28 +0100

    feat: generate types for supabase db

---
+++ types/supabase.ts
@@ -0,0 +1,60 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      links: {
+        Row: {
+          id: string
+          link_slug: string
+          owner: string
+          redirect_to: string
+        }
+        Insert: {
+          id?: string
+          link_slug: string
+          owner: string
+          redirect_to: string
+        }
+        Update: {
+          id?: string
+          link_slug?: string
+          owner?: string
+          redirect_to?: string
+        }
+      }
+      profiles: {
+        Row: {
+          id: string
+          username: string | null
+        }
+        Insert: {
+          id: string
+          username?: string | null
+        }
+        Update: {
+          id?: string
+          username?: string | null
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
