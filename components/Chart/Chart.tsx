b1adfa31f01679fdf9d763cc5287e6e0bc626cdc
witpiw <piweckiwit@gmail.com>
2023-03-24 17:52:38 +0100

    feat: add stats/[slug] page

--- components/Chart/Chart.tsx
+++ components/Chart/Chart.tsx
@@ -1,20 +1,23 @@
-import { useMemo } from 'react';
-import { BarChart , Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import { BarChart , Bar, XAxis, YAxis, Tooltip, Legend, Label } from 'recharts';
 
 import type { Database } from "../../types/supabase";
 
 interface IProps {
     data: Database["public"]["Tables"]["link_visits"]["Row"][]
+    width?: number
+    height?: number
 }
 
 function Chart(props: IProps) {
     return (
-        <BarChart width={600} height={300} data={props.data} >
+        <BarChart width={props.width || 600} height={props.height || 300} data={props.data} >
 			<XAxis dataKey="date" />
 			<YAxis />
+            <Legend verticalAlign="top" height={48}/>
 			<Bar dataKey="total_visits" barSize={30} fill="black"/>
             <Bar dataKey="unique_visits" barSize={30} fill="gray"/>
 			<Tooltip cursor={{fill: 'transparent'}}/>
+            <Label value="Visits per day" offset={0} position="insideBottom" />
 		</BarChart>
     )
 }
--- pages/stats/[slug].tsx
+++ pages/stats/[slug].tsx
@@ -1,14 +1,65 @@
-import { getNextRedirect } from "../../utils";
-import { Database } from "../../types/supabase";
+const Chart = dynamic(() => import('../../components').then((mod) => mod.Chart), {
+	ssr: false,
+	loading: () => <Loading height={300}/>
+});
 
-import type { GetServerSidePropsContext } from "next";
+import { useMemo, useRef, useEffect } from 'react';
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { Flex, Heading } from '@chakra-ui/react';
+import dynamic from 'next/dynamic';
+import { useWindowSize } from 'usehooks-ts';
 
-type IProps = Database["public"]["Tables"]["links"]["Row"] &
-	Database["public"]["Tables"]["link_visits"]["Row"];
+import { Loading } from '../../components';
+import { getNextRedirect } from "../../utils";
+
+import type { Database } from "../../types/supabase";
+import type { GetServerSidePropsContext } from "next";
+import Link from 'next/link';
+
+type IProps = Database["public"]["Tables"]["links"]["Row"] & {link_visits:Database["public"]["Tables"]["link_visits"]["Row"][] };
+
 
 function Stats(props: IProps) {
-	return <pre>{JSON.stringify(props, null, 4)}</pre>;
+	const {width} = useWindowSize()
+	const link = useRef("")
+
+	const data = useMemo(() => {
+		return [...props.link_visits].sort((a,b) => {
+			const aDate = new Date(a.date)
+			const bDate = new Date(b.date)
+
+			return aDate.getTime() + bDate.getTime()
+		})
+	}, [props.link_visits])
+
+	const totalVisist = useMemo(() => {
+		return props.link_visits.reduce((prev,curr) => {
+			return prev + curr.total_visits
+		}, 0)
+	}, [props.link_visits])
+
+	const uniqueVisist = useMemo(() => {
+		return props.link_visits.reduce((prev,curr) => {
+			return prev + curr.unique_visits
+		}, 0)
+	}, [props.link_visits])
+
+	useEffect(() => {
+		link.current = window.location.origin + "/" + props.link_slug
+	}, [props])
+
+	return (
+		<>
+			<Flex width="100%" direction={"column"} justify={"center"} align="center" gap={"3rem"}>
+				<Heading size={"lg"} textAlign={"center"}>Link: <Link style={{textDecoration: "underline"}} href={link.current}>{link.current}</Link></Heading>
+				<Flex gap={"2rem"}>
+					<Heading size={"md"} textAlign={"center"}>Total visits: {totalVisist}</Heading>
+					<Heading size={"md"} textAlign={"center"}>Unique visits: {uniqueVisist}</Heading>
+				</Flex>
+				<Chart width={width > 1000 ? width / 2 : 0.8 * width} data={data}/>
+			</Flex>
+		</>
+	)
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
